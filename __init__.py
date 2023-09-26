561a6a49d0636a16fd249eb2d9bc9892c7046d2d
Egor Fadeev <fad.egor05@gmail.com>
2023-09-26 13:43:05 +0400

    #2 Commit
    - Structure of project was created

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+from database import db
+from views import main
+
+def make_app(config_file = 'settings.py'):
+    app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    db.init_app(app)
+    app.register_blueprint(main)
+    app.run()
+
+    return app
+
+
+if __name__ == '__main__':
+    make_app()
